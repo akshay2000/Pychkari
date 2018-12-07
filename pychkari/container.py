0a90f040762a9d66e9b137f073fdda13af3984c7
Akshay Zade <akshay2000@hotmail.com>
2018-12-07 17:12:50 +0530

    Added support for factory functions

--- pychkari/container.py
+++ pychkari/container.py
@@ -1,65 +1,87 @@
 import inspect
 from inspect import Parameter
 
-__registry = {}
 
+class Container:
 
-def register(name: str, thing):
-    if name in __registry:
-        raise ValueError("Service {0} is already registered".format(name))
-    __registry[name] = thing
+    def __init__(self):
+        self.__registry = {}
+        self.__values = {}
 
+    def register_class(self, clazz):
+        if not inspect.isclass(clazz):
+            raise ValueError("Only classes can be registered without explicit service name")
+        class_name = clazz.__name__
+        self.register(class_name, clazz)
 
-def is_registered(name: str) -> bool:
-    return name in __registry
+    def register(self, name: str, thing):
+        if name in self.__registry:
+            raise ValueError("Service {0} is already registered".format(name))
+        self.__registry[name] = thing
 
+    def is_registered(self, name: str) -> bool:
+        return name in self.__registry
 
-def get(service_name: str):
-    if not is_registered(service_name):
-        raise ValueError("Service {0} is not registered".format(service_name))
-    value = __get_value(service_name)
-    return value
+    def get(self, service_name: str):
+        if not self.is_registered(service_name):
+            raise ValueError("Service {0} is not registered".format(service_name))
+        value = self.__get_value(service_name)
+        return value
 
+    def __get_value(self, name: str):
+        if name not in self.__values:
+            thing = self.__registry[name]
+            if inspect.isclass(thing):
+                self.__values[name] = self.__instance_from_class(thing)
+            if inspect.isfunction(thing):
+                self.__values[name] = self.__instance_from_function(thing)
 
-__values = {}
+        return self.__values[name]
 
+    def __instance_from_class(self, clazz):
+        return self.__instance_from_callable(clazz)
 
-def __get_value(name: str):
-    if name not in __values:
-        thing = __registry[name]
-        if inspect.isclass(thing):
-            __values[name] = __instance_from_class(thing)
+    def __instance_from_function(self, f):
+        return self.__instance_from_callable(f)
 
-    return __values[name]
+    def __instance_from_callable(self, c):
+        if not callable(c):
+            raise ValueError("Given thing {0} is not callable".format(c))
 
+        signature = inspect.signature(c)
+        params = signature.parameters
+        args = []
+        for param_name, param in params.items():
+            annotation = param.annotation
+            service_name = annotation
+            if annotation == Parameter.empty:
+                service_name = Container.arg_name_to_service_name(param.name)
 
-def __instance_from_class(clazz):
-    signature = inspect.signature(clazz)
-    params = signature.parameters
-    args = []
-    for param_name, param in params.items():
-        annotation = param.annotation
-        service_name = annotation
-        if annotation == Parameter.empty:
-            service_name = __arg_name_to_service_name(param.name)
+            if self.is_registered(service_name):
+                value = self.get(service_name)
+            elif param.default != Parameter.empty:
+                value = param.default
+            else:
+                raise ValueError("Service {0} is not registered and arg {1} doesn't have a default value".
+                                 format(service_name, param_name))
+            args.append(value)
 
-        if is_registered(service_name):
-            value = get(service_name)
-        elif param.default != Parameter.empty:
-            value = param.default
-        else:
-            raise ValueError("Service {0} is not registered and arg {1} doesn't have a default value".
-                             format(service_name, param_name))
-        args.append(value)
+        return c(*args)
 
-    return clazz(*args)
+    @staticmethod
+    def arg_name_to_service_name(arg_name: str) -> str:
+        tokens = arg_name.split("_")
 
+        def cap_first(word):
+            return word[:1].upper() + word[1:]
 
-def __arg_name_to_service_name(arg_name: str) -> str:
-    tokens = arg_name.split("_")
+        capitalized_tokens = list(map(cap_first, tokens))
+        return "".join(capitalized_tokens)
 
-    def cap_first(word):
-        return word[:1].upper() + word[1:]
+    __instance = None
 
-    capitalized_tokens = list(map(cap_first, tokens))
-    return "".join(capitalized_tokens)
+    @staticmethod
+    def instance():
+        if Container.__instance is None:
+            Container.__instance = Container()
+        return Container.__instance
--- test/pychkari_test.py
+++ test/pychkari_test.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 import pytest
 
-from pychkari import container
+from pychkari.container import Container
 
 
 class A:
-    def __init__(self, depOne, dep_two: "DepTwo", const_dep=3):
+    def __init__(self, depOne, second_dep: "DepTwo", const_dep=3):
         self.dep1 = depOne
-        self.dep2 = dep_two
+        self.dep2 = second_dep
         self.const_d = const_dep
 
 
@@ -22,7 +22,42 @@ class DepTwo:
         self.timestamp = datetime.now()
 
 
-def test_class_instance():
+class B:
+    def __init__(self, a, dep_one):
+        self.a = a
+        self.dep1 = dep_one
+
+
+def test_from_class():
+    container = Container()
+    container.register("DepOne", DepOne)
+    o = container.get("DepOne")
+    assert isinstance(o, DepOne)
+    o_again = container.get("DepOne")
+    assert o == o_again
+
+
+def test_from_factory():
+    container = Container()
+    const_dep = 42
+
+    def factory_a():
+        return A(DepOne(), DepTwo(), const_dep)
+
+    container.register("AWithFactory", factory_a)
+    container.register("AWithoutFactory", A)
+    container.register("DepOne", DepOne)
+    container.register("DepTwo", DepTwo)
+
+    a_from_factory = container.get("AWithFactory")
+    a_without_factory = container.get("AWithoutFactory")
+
+    assert a_from_factory.const_d == const_dep
+    assert a_without_factory != a_from_factory
+
+
+def test_injection():
+    container = Container()
     container.register("A", A)
     container.register("DepOne", DepOne)
     container.register("DepTwo", DepTwo)
@@ -32,6 +67,22 @@ def test_class_instance():
     assert a.dep1 == d1
     assert a.dep2 == d2
     assert a.dep1 != DepOne()
+
+
+def test_multi_level_injection():
+    container = Container()
+    container.register_class(B)
+    container.register_class(A)
+    container.register_class(DepOne)
+    container.register_class(DepTwo)
+
+    b = container.get("B")
+    a = container.get("A")
+    dep1 = container.get("DepOne")
+
+    assert b.a == a
+    assert b.dep1 == dep1
+    assert a.dep1 == dep1
 
 
 arg_names = [
@@ -45,4 +96,4 @@ arg_names = [
 
 @pytest.mark.parametrize("arg_name,service_name", arg_names)
 def test_arg_name_to_service_name(arg_name, service_name):
-    assert container.__arg_name_to_service_name(arg_name) == service_name
+    assert Container.arg_name_to_service_name(arg_name) == service_name
