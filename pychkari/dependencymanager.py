64dc80a404b899391579192dc17c7cfd54e10fc0
Akshay Zade <akshay2000@hotmail.com>
2018-12-09 20:36:26 +0530

    Added dependency graph checker

---
+++ pychkari/dependencymanager.py
@@ -0,0 +1,38 @@
+from errors import CyclicDependencyError
+
+
+class Node:
+    def __init__(self, name: str, dependencies: list = None):
+        self.name = name
+        self.dependencies = dependencies
+        self.is_okay = None
+
+
+def check_graph(all_nodes: dict, node: Node, current_chain: list = None):
+    chain = current_chain or []
+    indent = "  " * len(chain)
+    print(indent + "Node: {0} Chain: {1}".format(node.name, chain))
+    if node.name in chain:
+        return False
+
+    if node.is_okay:
+        print(indent + " Node was already evaluated")
+        return True
+
+    if not node.dependencies:
+        print(indent + " Leaf node")
+        return True
+
+    print(indent + " Evaluating dependencies")
+    chain.append(node.name)
+
+    for d in node.dependencies:
+        current_node = all_nodes[d]
+        is_okay = check_graph(all_nodes, current_node, chain)
+        if not is_okay:
+            dep_chain = chain + [d]
+            raise CyclicDependencyError(d, dep_chain)
+        current_node.is_okay = is_okay
+
+    chain.pop()
+    return True
